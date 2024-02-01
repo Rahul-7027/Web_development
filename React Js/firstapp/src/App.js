93aa3c431a922d2e1f50a9cfb9a76b3e03ef8d66
rahul-7027 <rahulbhati0202@gmail.com>
2024-02-01 21:35:49 +0530

    firstapp react js

--- React Js/firstapp/src/App.js
+++ React Js/firstapp/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Navbar from './component/navbar';
+import Footer from './component/Footer';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    setCount(count + 1);
+  }
+  const decrement = () => {
+    setCount(count - 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar logo={"Happy"}/>
+      {count}
+      <div className="button ">
+        <button type="button" style={{ margin: "20px" }} onClick={increment}>Increment</button>
+        <button type="button" disabled={count === 0} onClick={decrement}>Decrement</button>
+        {/* Disabled Pointing for 
+        1. disabled={count === 0}
+        2. disabled={count <= 0} */}
+        {/* <Footer /> */}
+      </div>
     </div>
   );
 }
